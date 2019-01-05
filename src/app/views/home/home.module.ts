88f76469d90084c56b489ba583e9e917cee7218e
BuiNguyenHoangAnh <hoanganhbui099@gmail.com>
2019-01-05 16:50:01 +0700

    add content for home page: jars percent base on month

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { MaterialModule } from 'src/app/material-module';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
   ],
   exports: [
     HomeComponent,
