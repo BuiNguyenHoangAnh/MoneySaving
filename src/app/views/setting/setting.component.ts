ad4db59cce2ea853864b4981872834b8fef7a861
BuiNguyenHoangAnh <hoanganhbui099@gmail.com>
2019-01-05 23:51:48 +0700

    add setting page: change percent of jars, show pie chart

--- src/app/views/setting/setting.component.ts
+++ src/app/views/setting/setting.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import * as CanvasJS from './../../.;
 
 @Component({
   selector: 'app-setting',
@@ -10,6 +11,30 @@ export class SettingComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+// window.onload = function() {
+
+    var chart = new CanvasJS.Chart("chartContainer", {
+      animationEnabled: true,
+      title: {
+        // text: "Desktop Search Engine Market Share - 2016"
+      },
+      data: [{
+        type: "pie",
+        startAngle: 240,
+        yValueFormatString: "##0.00'%'",
+        indexLabel: "{label} {y}",
+        dataPoints: [
+          {y: 79.45, label: "Education"},
+          {y: 7.00, label: "Neccessaties"},
+          {y: 7.00, label: "Play"},
+          {y: 4.91, label: "Give"},
+          {y: 1.00, label: "Long time saving"},
+          {y: 0.63, label: "Freedom Financial"}
+        ]
+      }]
+    });
+    chart.render();
   }
+}
 
 }
--- src/app/views/views-routing.module.ts
+++ src/app/views/views-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { ViewsComponent } from './views.component';
 import { HomeComponent } from './home/home.component';
 import { LogInComponent } from './log-in/log-in.component';
+import { SettingComponent } from './setting/setting.component';
 
 const routes: Routes = [
     {
@@ -11,7 +12,8 @@ const routes: Routes = [
         children: [
             // { path: '', component: LogInComponent },
             // { path: 'home', component: HomeComponent },
-            { path: '', component: HomeComponent },
+            // { path: 'setting', component: SettingComponent },
+            { path: '', component: SettingComponent },
         ]
     }
 ];
